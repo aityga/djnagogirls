ba625637d8c29aab106059b5003ef8e2b463ffbb
aityga <sealaa3@gmail.com>
2021-10-13 01:54:18 +0600

    o

--- blog/forms.py
+++ blog/forms.py
@@ -1,10 +1,10 @@
-from django import forms
+# from django import forms
 
-from .models import Post
+# from .models import Post
 
-class PostForm(forms.ModelForm):
+# class PostForm(forms.ModelForm):
 
-    class Meta:
-        model = Post
-        fields = ('title', 'text',)
+#     class Meta:
+#         model = Post
+#         fields = ('title', 'text',)
         
